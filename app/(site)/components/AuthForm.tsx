c85ca8832ec2b07f3b478a076436d30de48d5c99
Yadav106 <sahil.jp.yadav@gmail.com>
2023-08-16 01:38:35 +0530

    completed authentication

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,9 @@ import { BsGithub, BsGoogle } from "react-icons/bs";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -39,18 +42,42 @@ const AuthForm = () => {
         setIsLoading(true);
 
         if(variant === 'REGISTER') {
-            // Axios register
+            axios.post('/api/register', data)
+            .catch(() => toast.error('Something went wrong'))
+            .finally(() => setIsLoading(false))
         }
 
         if (variant === 'LOGIN') {
-            // NextAuth login
+            signIn('credentials', {
+                ...data,
+                redirect: false,
+            })
+            .then((callback) => {
+                if (callback?.error) {
+                    toast.error('Invalid credentials')
+                }
+
+                if (callback?.ok && !callback?.error) {
+                    toast.success('Logged in!')
+                }
+            })
+            .finally(() => setIsLoading(false))
         }
     }
 
     const socialAction = (action: string) => {
         setIsLoading(true);
+        signIn(action, { redirect: false })
+        .then((callback) => {
+            if (callback?.error) {
+                toast.error('Something went wrong')
+            }
 
-        // NextAuth social login
+            if (callback?.ok && !callback?.error) {
+                toast.success('Logged in!')
+            }
+        })
+        .finally(() => setIsLoading(false))
     }
 
     return ( 
